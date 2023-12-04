a298fea131c04b4d668a83e01698f9446c9a780a
Jishnu M M <91713896+ThePyWizard@users.noreply.github.com>
2023-12-04 20:40:50 +0530

    added files

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+app.whenReady().then(() => {
+    //create a window
+    const myWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    //load a window
+    myWindow.loadFile('index.html');
+});
